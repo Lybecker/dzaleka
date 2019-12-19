40b09807181cf4c62bc3956b29ba998767196bd4
Anders Lybecker <anders@lybecker.com>
2019-12-19 10:23:17 +0100

    Linq and Anonymous functions

---
+++ LinqSample/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace LinqSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] scores = new int[] { 97, 92, 81, 60 };
+
+            // Linq query expression. See more at https://linqsamples.com/
+            IEnumerable<int> scoreQuery =
+                from score in scores
+                where score > 80
+                select score;
+
+            // Lambda expression
+            var scoreQuery2 = scores.Where(x => x > 80);
+
+            // Execute the query.
+            foreach (int i in scoreQuery)
+            {
+                Console.Write(i + " ");
+            }
+
+        }
+    }
+}
